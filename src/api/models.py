113f91c9d2423f8ffd3d2234e005629d42dd89af
Emaxpm <140901128+Emaxpm@users.noreply.github.com>
2023-11-30 03:11:55 +0000

    hice un post y un get

--- src/api/models.py
+++ src/api/models.py
@@ -5,15 +5,29 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    password = db.Column(db.String(250), unique=False, nullable=False)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    last_name = db.Column(db.String(50), unique=False, nullable=False)
+    user_name = db.Column(db.String(50), unique=False, nullable=False)
+    country = db.Column(db.String(50), unique=False, nullable=False)
+    phone = db.Column(db.String(20), unique=False, nullable=False)
+    address = db.Column(db.String(150), unique=False, nullable=False)
+    date = db.Column(db.Date, unique=False, nullable=False)
+    
     def __repr__(self):
         return f'<User {self.email}>'
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
+            "password": self.password,
+            "last_name": self.last_name,
+            "user_name": self.user_name,
+            "country": self.country,
+            "phone": self.phone,
+            "address": self.address,
+            "date": self.date.strftime('%Y-%m-%d %H:%M:%S')
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,37 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import json
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+@api.route('/sign_up', methods=['POST'])
+def create_one_user():
+    body = json.loads(request.data)
+    new_user = User(
+        name = body["name"],
+        last_name = body["last_name"],
+        email = body["email"],
+        password = body["password"],
+        user_name= body["user_name"],
+        country = body["country"],
+        phone = body["phone"],
+        address = body["address"],
+        date= body["date"],
+        is_active = True
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"msg": "user created succesfull", "user_added": new_user}), 200
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/user/<int:user_id>', methods=['GET'])
+def get_one_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({"msg": f"user with id {user_id} not found"}), 404
+    serialized_user = user.serialize()
+    return serialized_user, 200
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
