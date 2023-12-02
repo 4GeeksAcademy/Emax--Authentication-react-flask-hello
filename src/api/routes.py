8897e397b3f89455f187f5606dfa904187e41d73
Emaxpm <140901128+Emaxpm@users.noreply.github.com>
2023-12-02 00:22:56 +0000

    encriptar contraseña

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,20 +6,26 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 import json
+from datetime import timedelta
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
+from flask import Flask, request, jsonify, url_for, send_from_directory
 
 api = Blueprint('api', __name__)
-
+bcrypt = Bcrypt()
 # Allow CORS requests to this API
 CORS(api)
 
 @api.route('/sign_up', methods=['POST'])
 def create_one_user():
     body = json.loads(request.data)
+    raw_password = request.json.get('password')
+    password_hash = bcrypt.generate_password_hash(raw_password).decode('utf-8')
     new_user = User(
         name = body["name"],
         last_name = body["last_name"],
         email = body["email"],
-        password = body["password"],
+        password = password_hash,
         user_name= body["user_name"],
         country = body["country"],
         phone = body["phone"],
@@ -29,7 +35,21 @@ def create_one_user():
     )
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({"msg": "user created succesfull", "user_added": new_user}), 200
+
+    ok_to_share = {
+        "name": body["name"],
+        "last_name": body["last_name"],
+        "email": body["email"],
+        "password": body["password"],
+        "user_name": body["user_name"],
+        "country": body["country"],
+        "phone": body["phone"],
+        "address": body["address"],
+        "date": body["date"],
+        "is_active": True
+    }
+
+    return jsonify({"msg": "user created succesfull", "user_added": ok_to_share}), 200
 
 @api.route('/user/<int:user_id>', methods=['GET'])
 def get_one_user(user_id):
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -37,62 +37,66 @@ export const Home = () => {
 
 	return (
 		<div className="container mt-5">
-			
-			<form className="row g-3">
-			<div className="col-md-6">
-				<label for="inputEmail4" className="form-label">Email</label>
-				<input type="email" className="form-control" id="inputEmail4" value={email} onChange={(e)=> setEmail(e.target.value)}/>
-			</div>
-			<div className="col-md-6">
-				<label for="inputPassword4" className="form-label">Password</label>
-				<input type="password" className="form-control" id="inputPassword4" value={password} onChange={(e)=> setPassword(e.target.value)}/>
-			</div>
-			<div className="col-12">
-				<label for="inputAddress" className="form-label">Name</label>
-				<input type="text" className="form-control" id="inputAddress" value={name} onChange={(e)=> setName(e.target.value)}/>
-			</div>
-			<div className="col-12">
-				<label for="inputAddress2" className="form-label">Last Name</label>
-				<input type="text" className="form-control" id="inputAddress2" value={lastName} onChange={(e)=> setLastName(e.target.value)}/>
-			</div>
 
-			<div className="col-md-6">
-				<label for="inputCity" className="form-label">User Name</label>
-				<input type="text" className="form-control" id="inputCity" value={userName} onChange={(e)=> setUserName(e.target.value)}/>
-			</div>
-
-			<div className="col-md-6">
-				<label for="inputCity" className="form-label">Country</label>
-				<input type="text" className="form-control" id="inputCity" value={country} onChange={(e)=> setCountry(e.target.value)}/>
-			</div>
-
-			<div className="col-md-6">
-				<label for="inputCity" className="form-label">Phone</label>
-				<input type="number" className="form-control" id="inputCity" value={phone} onChange={(e)=> setPhone(e.target.value)}/>
-			</div>
-
-			<div className="col-md-6">
-				<label for="inputCity" className="form-label">Address</label>
-				<input type="text" className="form-control" id="inputCity" value={address} onChange={(e)=> setAddress(e.target.value)}/>
-			</div>
-
-			
-			<div className="col-md-2">
-				<label for="inputZip" className="form-label">Date</label>
-				<input type="date" className="form-control" id="inputZip" value={date} onChange={(e)=> setDate(e.target.value)}/>
-			</div>
-			<div className="col-12">
-				<div className="form-check">
-				<input className="form-check-input" type="checkbox" id="gridCheck" value={isActive} onChange={(e)=> setIsActive(e.target.value)}/>
-				<label className="form-check-label" for="gridCheck">
-					Is active
-				</label>
+			<div className="form-container">
+				
+				<form className="row g-3">
+				<div className="col-md-6">
+					<label for="inputEmail4" className="form-label">Email:</label>
+					<input type="email" className="form-control inputs" id="inputEmail4" value={email} onChange={(e)=> setEmail(e.target.value)}/>
 				</div>
+				<div className="col-md-6">
+					<label for="inputPassword4" className="form-label">Password:</label>
+					<input type="password" className="form-control inputs" id="inputPassword4" value={password} onChange={(e)=> setPassword(e.target.value)}/>
+				</div>
+				<div className="col-4">
+					<label for="inputAddress" className="form-label">Name:</label>
+					<input type="text" className="form-control inputs" id="inputAddress" value={name} onChange={(e)=> setName(e.target.value)}/>
+				</div>
+				<div className="col-4">
+					<label for="inputAddress2" className="form-label">Last Name:</label>
+					<input type="text" className="form-control inputs" id="inputAddress2" value={lastName} onChange={(e)=> setLastName(e.target.value)}/>
+				</div>
+
+				<div className="col-md-4">
+					<label for="inputCity" className="form-label">User Name:</label>
+					<input type="text" className="form-control inputs" id="inputCity" value={userName} onChange={(e)=> setUserName(e.target.value)}/>
+				</div>
+
+				<div className="col-md-6">
+					<label for="inputCity" className="form-label">Country:</label>
+					<input type="text" className="form-control inputs" id="inputCity" value={country} onChange={(e)=> setCountry(e.target.value)}/>
+				</div>
+
+				<div className="col-md-6">
+					<label for="inputCity" className="form-label">Phone:</label>
+					<input type="number" className="form-control inputs" id="inputCity" value={phone} onChange={(e)=> setPhone(e.target.value)}/>
+				</div>
+
+				<div className="col-md-8">
+					<label for="inputCity" className="form-label">Address:</label>
+					<input type="text" className="form-control inputs" id="inputCity" value={address} onChange={(e)=> setAddress(e.target.value)}/>
+				</div>
+
+				
+				<div className="col-md-4">
+					<label for="inputZip" className="form-label">Date:</label>
+					<input type="date" className="form-control inputs inp-date" id="inputZip" value={date} onChange={(e)=> setDate(e.target.value)}/>
+				</div>
+				<div className="col-12">
+					<div className="form-check">
+					<input className="form-check-input" type="checkbox" id="gridCheck" value={isActive} onChange={(e)=> setIsActive(e.target.value)}/>
+					<label className="form-check-label" for="gridCheck">
+						Is active
+					</label>
+					</div>
+				</div>
+				<div className="col-12">
+					<button type="submit" className="btn singup" onClick={(e)=> handlerSignUp(e)}>Sign Up</button>
+				</div>
+				</form>
+
 			</div>
-			<div className="col-12">
-				<button type="submit" className="btn btn-primary" onClick={(e)=> handlerSignUp(e)}>Sign in</button>
-			</div>
-			</form>
 		
 		</div>
 	);
